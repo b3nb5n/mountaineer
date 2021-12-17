8d9af1a9db21be524f7be11c1fada1fa2db3a096
Ben Baldwin <benbaldwin000@gmail.com>
2021-12-16 21:33:15 -0700

    setup firebase hosting and routing

--- frontend/student/src/index.tsx
+++ frontend/student/src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
-	<React.StrictMode>Hello, world!</React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>Hello, student!</BrowserRouter>
+	</React.StrictMode>,
 	document.querySelector('#root')
 );
